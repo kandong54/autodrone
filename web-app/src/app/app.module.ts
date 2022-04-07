8e9b112f85453d256bd38340bf268cd4a1f0ed93
Kan Dong <kd46@rice.edu>
2022-04-07 03:05:17 -0500

    feat(web): add routing

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -10,6 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule
   ],
   providers: [],
