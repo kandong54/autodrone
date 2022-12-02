d11251c45cf50a266d4636260f44d35e19ef8115
Kan Dong <kan.dong54@gmail.com>
2022-12-02 12:37:14 -0600

    feat(rpi3): test

---
+++ rpi3/test.py
@@ -0,0 +1,15 @@
+#pip3 install grpcio-tools
+#python3 -m grpc_tools.protoc --python_out=. --pyi_out=. --grpc_python_out=. --proto_path=./protos drone.proto
+import grpc
+import time
+import drone_pb2
+import drone_pb2_grpc
+
+channel = grpc.insecure_channel('127.0.0.1:9090')
+stub = drone_pb2_grpc.DroneStub(channel)
+image_size = stub.GetImageSize(drone_pb2.Empty())
+print(image_size)
+for _ in range(10):
+    time.sleep(1)
+    box = stub.GetBox(drone_pb2.Empty())
+    print(box)
