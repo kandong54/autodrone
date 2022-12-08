bb1906dbbb33a1c2f9631ef2212cb1b946ee8e59
Kan Dong <kan.dong54@gmail.com>
2022-12-08 13:34:56 -0600

    docs(rpi3): comments

--- rpi3/test.py
+++ rpi3/test.py
@@ -5,11 +5,14 @@ import time
 import drone_pb2
 import drone_pb2_grpc
 
+# init connection
 channel = grpc.insecure_channel('127.0.0.1:9090')
 stub = drone_pb2_grpc.DroneStub(channel)
+# get image size
 image_size = stub.GetImageSize(drone_pb2.Empty())
 print(image_size)
 for _ in range(10):
     time.sleep(1)
+    # get bounding boxes
     box = stub.GetBox(drone_pb2.Empty())
     print(box)
